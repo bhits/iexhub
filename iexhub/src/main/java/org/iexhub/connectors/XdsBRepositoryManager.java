200bd8132e00425c2496098951a07d13a3a33620
Shruti Rao <shruti.rao@feisystems.com>
2017-01-12 13:25:17 -0500

    Set externalIdentifier value to OID instead of documentID
    #12359: As a developer, I will have implemented FHIR Consent STU 3 in IExHub

--- iexhub/src/main/java/org/iexhub/connectors/XdsBRepositoryManager.java
+++ iexhub/src/main/java/org/iexhub/connectors/XdsBRepositoryManager.java
@@ -1684,7 +1684,8 @@ public class XdsBRepositoryManager
 			externalIdentifierUniqueId.setRegistryObject("urn:uuid:" + newDocumentUuid.toString());
 			externalIdentifierUniqueId.setIdentificationScheme(extrinsicObjectExternalIdentifierUniqueIdIdentificationScheme);
 
-			externalIdentifierUniqueId.setValue(documentIdToUse);
+			//externalIdentifierUniqueId.setValue(documentIdToUse);
+			externalIdentifierUniqueId.setValue(getOid());
 
 			text = new InternationalStringType();
 			localizedText = new LocalizedStringType();
@@ -1927,5 +1928,25 @@ public class XdsBRepositoryManager
 			}
 		});
 		return xPath;
+
+	}
+
+	private String getOid() {
+		final UUID uuid = UUID.randomUUID();
+		String id = String.valueOf(uuid);
+
+		id = id.replace("-", ".");
+
+		id = id.replace("a", "10");
+		id = id.replace("b", "11");
+		id = id.replace("c", "12");
+		id = id.replace("d", "13");
+		id = id.replace("e", "14");
+		id = id.replace("f", "15");
+
+		// Removes leading zeroes
+		id = id.replaceFirst("^0+(?!$)", "");
+
+		return id;
 	}
 }
